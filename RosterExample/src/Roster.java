fd03e452cd29f38e85816057716bb6e16031723b
danawen <danawenster@gmail.com>
2019-02-06 23:58:05 -0600

    RosterExample initial commit

---
+++ RosterExample/src/Roster.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Roster {
+	private List<Employee> employeeList;
+	
+	public Roster() {
+		employeeList = new ArrayList<>();
+	}
+	
+	public List<Employee> getEmployeeList() {
+		return employeeList;
+	}
+	
+	public void setEmployeeList(List<Employee> employees) {
+		employeeList = employees;
+	}
+	
+	public void addEmployee(Employee toAdd) {
+		employeeList.add(toAdd);
+	}
+}
